c9733c44e4eca672c53fe19e0c245210c815bdfa
Ujjwols <shrestha.ujjwol1@gmail.com>
2025-07-15 03:26:31 +0545

    public complain fixes

--- frontend/Userside/src/components/Profile.tsx
+++ frontend/Userside/src/components/Profile.tsx
@@ -244,7 +244,7 @@ const Profile = () => {
           </Button>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
           {/* Personal Information Card */}
           <div className="lg:col-span-1">
             <Card>
